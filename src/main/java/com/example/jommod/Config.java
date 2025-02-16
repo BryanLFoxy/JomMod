58161e691a465f6e3e82cfb0189cdabcbac61221
FoxyDias <ludiwebjeux@hotmail.fr>
2025-02-16 20:19:59 +0100

    update path

--- src/main/java/com/example/jommod/Config.java
+++ src/main/java/com/example/jommod/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.example.jommod;
 
 import java.util.List;
 import java.util.Set;
--- src/main/java/com/example/jommod/ExampleMod.java
+++ src/main/java/com/example/jommod/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.example.jommod;
 
 import org.slf4j.Logger;
 
